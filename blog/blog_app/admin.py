731061923f118093ecfe4917609f19584881a4ee
SirValentin <valentincarvajalm@gmail.com>
2022-09-05 11:17:42 -0400

    publicidad en admin panel

--- blog/blog_app/admin.py
+++ blog/blog_app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from blog_app.models import Article
+from blog_app.models import Article, Advertising
 
 @admin.register(Article)
 class Articles_admin(admin.ModelAdmin):
     list_display = ['title', 'content', 'author']
+
+@admin.register(Advertising)
+class Advertising(admin.ModelAdmin):
+    list_display = ['company', 'title', 'description', 'is_active', 'image']
